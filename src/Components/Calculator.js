77c5f79b2b2b3cfa5e008c2a4683377a0f479c74
Favourezeugwa <favourezeugwa@gmail.com>
2022-06-29 18:15:23 +0100

    develop UI of the calculator

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -9,26 +9,28 @@ class Calculator extends React.Component {
   render() {
     return (
       <div className="calculator-container">
-        <button type="button" className="solution btn-darkgrey">0</button>
-        <button type="button" className="btn-lightgrey">AC</button>
-        <button type="button" className="btn-lightgrey">+/-</button>
-        <button type="button" className="btn-lightgrey">%</button>
-        <button type="button" className="btn-orange">+</button>
-        <button type="button" className="btn-lightgrey">7</button>
-        <button type="button" className="btn-lightgrey">8</button>
-        <button type="button" className="btn-lightgrey">9</button>
-        <button type="button" className="btn-orange">X</button>
-        <button type="button" className="btn-lightgrey">4</button>
-        <button type="button" className="btn-lightgrey">5</button>
-        <button type="button" className="btn-lightgrey">6</button>
-        <button type="button" className="btn-orange">-</button>
-        <button type="button" className="btn-lightgrey">1</button>
-        <button type="button" className="btn-lightgrey">2</button>
-        <button type="button" className="btn-lightgrey">3</button>
-        <button type="button" className="btn-orange">+</button>
-        <button type="button" className="btn-lightgrey">0</button>
-        <button type="button" className="btn-lightgrey">&sdot;</button>
-        <button type="button" className="btn-orange">=</button>
+        <input type="text" className="calculator-screen" value="0" />
+        <div className="calculator-keys">
+          <button type="button" className="btn-lightgrey">AC</button>
+          <button type="button" className="btn-lightgrey">+/-</button>
+          <button type="button" className="btn-lightgrey">%</button>
+          <button type="button" className="btn-orange">+</button>
+          <button type="button" className="btn-lightgrey">7</button>
+          <button type="button" className="btn-lightgrey">8</button>
+          <button type="button" className="btn-lightgrey">9</button>
+          <button type="button" className="btn-orange">*</button>
+          <button type="button" className="btn-lightgrey">4</button>
+          <button type="button" className="btn-lightgrey">5</button>
+          <button type="button" className="btn-lightgrey">6</button>
+          <button type="button" className="btn-orange">-</button>
+          <button type="button" className="btn-lightgrey">1</button>
+          <button type="button" className="btn-lightgrey">2</button>
+          <button type="button" className="btn-lightgrey">3</button>
+          <button type="button" className="btn-orange">+</button>
+          <button type="button" className="btn-lightgrey item0">0</button>
+          <button type="button" className="btn-lightgrey">&sdot;</button>
+          <button type="button" className="btn-orange">=</button>
+        </div>
       </div>
     );
   }
