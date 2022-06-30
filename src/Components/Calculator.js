e765266643a173094d47dd2090291023acfacbff
Favourezeugwa <favourezeugwa@gmail.com>
2022-06-30 13:06:29 +0100

    Add event handler to calculator.js

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -5,32 +5,38 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState((state) => calculate(state, event.target.textContent));
   }
 
   render() {
+    const { total, operation, next } = this.state;
     return (
       <div className="calculator-container">
-        <input type="text" className="calculator-screen" value="0" />
+        <input type="text" className="calculator-screen" value={(total || '') + (operation || '') + (next || '')} />
         <div className="calculator-keys">
-          <button type="button" className="btn-lightgrey">AC</button>
-          <button type="button" className="btn-lightgrey">+/-</button>
-          <button type="button" className="btn-lightgrey">%</button>
-          <button type="button" className="btn-orange">+</button>
-          <button type="button" className="btn-lightgrey">7</button>
-          <button type="button" className="btn-lightgrey">8</button>
-          <button type="button" className="btn-lightgrey">9</button>
-          <button type="button" className="btn-orange">x</button>
-          <button type="button" className="btn-lightgrey">4</button>
-          <button type="button" className="btn-lightgrey">5</button>
-          <button type="button" className="btn-lightgrey">6</button>
-          <button type="button" className="btn-orange">-</button>
-          <button type="button" className="btn-lightgrey">1</button>
-          <button type="button" className="btn-lightgrey">2</button>
-          <button type="button" className="btn-lightgrey">3</button>
-          <button type="button" className="btn-orange">+</button>
-          <button type="button" className="btn-lightgrey item0">0</button>
-          <button type="button" className="btn-lightgrey">&sdot;</button>
-          <button type="button" className="btn-orange">=</button>
+          <button type="button" className="btn-lightgrey" onClick={this.handleClick}>AC</button>
+          <button type="button" className="btn-lightgrey" onClick={this.handleClick}>+/-</button>
+          <button type="button" className="btn-lightgrey" onClick={this.handleClick}>%</button>
+          <button type="button" className="btn-orange" onClick={this.handleClick}>÷</button>
+          <button type="button" className="btn-lightgrey" onClick={this.handleClick}>7</button>
+          <button type="button" className="btn-lightgrey" onClick={this.handleClick}>8</button>
+          <button type="button" className="btn-lightgrey" onClick={this.handleClick}>9</button>
+          <button type="button" className="btn-orange" onClick={this.handleClick}>x</button>
+          <button type="button" className="btn-lightgrey" onClick={this.handleClick}>4</button>
+          <button type="button" className="btn-lightgrey" onClick={this.handleClick}>5</button>
+          <button type="button" className="btn-lightgrey" onClick={this.handleClick}>6</button>
+          <button type="button" className="btn-orange" onClick={this.handleClick}>-</button>
+          <button type="button" className="btn-lightgrey" onClick={this.handleClick}>1</button>
+          <button type="button" className="btn-lightgrey" onClick={this.handleClick}>2</button>
+          <button type="button" className="btn-lightgrey" onClick={this.handleClick}>3</button>
+          <button type="button" className="btn-orange" onClick={this.handleClick}>+</button>
+          <button type="button" className="btn-lightgrey item0" onClick={this.handleClick}>0</button>
+          <button type="button" className="btn-lightgrey" onClick={this.handleClick}>&sdot;</button>
+          <button type="button" className="btn-orange" onClick={this.handleClick}>=</button>
         </div>
       </div>
     );
