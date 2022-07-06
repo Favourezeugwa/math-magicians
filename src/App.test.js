1dff581315e173ba168dc9c4937cd9dcf8aa6743
Felix Ouma <fatonoh@gmail.com>
2022-07-07 02:25:33 +0300

    Fix linter error in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/quote/i);
-  const linkElementTwo = screen.getByText(/calculator/i)
+  const linkElementTwo = screen.getByText(/home/i);
   expect(linkElement).toBeInTheDocument();
   expect(linkElementTwo).toBeInTheDocument();
 });
