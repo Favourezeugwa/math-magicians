421044ee06621236f4e13687441a28357ec207b7
Felix Ouma <fatonoh@gmail.com>
2022-07-07 02:23:03 +0300

    Fix App.test.js test error

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/quote/i);
+  const linkElementTwo = screen.getByText(/calculator/i)
   expect(linkElement).toBeInTheDocument();
+  expect(linkElementTwo).toBeInTheDocument();
 });
