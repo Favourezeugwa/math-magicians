0ec54ab56f957623ca8b89cc55687e5c3f7b9ca5
Felix Ouma <fatonoh@gmail.com>
2022-07-07 02:28:45 +0300

    Change test description in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Test UI text', () => {
   render(<App />);
   const linkElement = screen.getByText(/quote/i);
   const linkElementTwo = screen.getByText(/home/i);
